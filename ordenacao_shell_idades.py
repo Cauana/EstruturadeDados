5a57d07f69b78268d7782ab269b9d7feca99285b
Cauana Dias <cauana.dias@hotmail.com>
2023-11-06 21:16:32 -0300

    update

--- ordenacao_shell_idades.py
+++ ordenacao_shell_idades.py
@@ -15,7 +15,7 @@ def ordenacao_shell(alturas):
 
 if __name__ == "__main__":
 
-    alturas = [1.50,1.45,2.0,1.65,1.70,1.80,1.43]
+    alturas = [1.50,1.45,2.0,1.65,1.70,1.80,1.43,1.58,1.90,1.75,1.83]
     ordenacao_shell(alturas)
     print(alturas)
 
