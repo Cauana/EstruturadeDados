86ad49a842cc812e9f2b0f9b90cc2fcd2ad202b0
Cauana Dias <cauana.dias@hotmail.com>
2023-11-10 20:28:28 -0300

    update

--- ordenacao_shell_alturas.py
+++ ordenacao_shell_alturas.py
@@ -2,6 +2,7 @@
 
 def ordenacao_shell(alturas):
     divisao = len(alturas) // 2
+    print(divisao)
     while divisao > 0:
         for i in range(divisao, len(alturas)):
             temp = alturas[i]
@@ -9,13 +10,16 @@ def ordenacao_shell(alturas):
             while j >= divisao and alturas[j - divisao] > temp:
                 alturas[j] = alturas[j - divisao]
                 j -= divisao
+                # print(alturas)
             alturas[j] = temp
+            print(alturas)
         divisao //= 2
+        print(divisao)
 
 
 if __name__ == "__main__":
 
-    alturas = [1.50,1.45,2.0,1.65,1.70,1.80,1.43,1.58,1.90,1.75,1.83]
+    alturas = [1.50,1.45,2.0,1.65,1.70,1.80,1.43]
     ordenacao_shell(alturas)
     print(alturas)
 
